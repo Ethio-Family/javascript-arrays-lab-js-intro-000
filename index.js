2d77b38e0ab6adbb5953f73670f289477e0baf1b
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-01 19:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ function appendKitten(name) {
 
 function prependKitten(name) {
 
-  var array1 = new array();
-  array1 = name
+  var array1 = [name]
   var array2 = kittens
   var array3 = array1.concat(array2);
   return array3;
